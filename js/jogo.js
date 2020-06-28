e600019c7005a07d5045d553481fb9e636fbb61a
Brendow Lincoln <linconlbrendow@gmail.com>
2020-06-28 00:44:28 -0300

    Colocando função para controlar os pontos de vidas.

--- js/jogo.js
+++ js/jogo.js
@@ -2,6 +2,7 @@
 
 var altura = 0;
 var largura = 0;
+var vidas = 1;
 
 function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
@@ -19,6 +20,16 @@ function posicaoRandomica() {
     //remover mosquito anterior (caso exsita)
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove();
+
+        if(vidas > 3) {
+            alert('Interromper o jogo (game over)');
+            window.location.reload();
+        } else{
+            document.getElementById('v' + vidas).src="/img/coracao_vazio.png";
+            vidas++;
+
+        }
+               
     }
 
     var posicaoX = Math.floor(Math.random() * largura) - 90;
@@ -39,6 +50,9 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px';
     mosquito.style.position = 'absolute';
     mosquito.id = 'mosquito'
+    mosquito.onclick = function() {
+        this.remove();
+    }
 
     document.body.appendChild(mosquito);
     
