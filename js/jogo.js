6447a1a3e7bfc73034c4869e3be5ac8307966b01
Brendow Lincoln <linconlbrendow@gmail.com>
2020-06-27 22:23:11 -0300

    Criando objeto mosca do jogo e gerando posicionamento aleatório.

--- js/jogo.js
+++ js/jogo.js
@@ -1,3 +1,5 @@
+//Adquirindo altura e largura da página
+
 var altura = 0;
 var largura = 0;
 
@@ -5,8 +7,33 @@ function ajustaTamanhoPalcoJogo() {
     altura = window.innerHeight;
     largura = window.innerWidth;
 
-    console.log(altura, largura);
+    console.log(largura, altura);
 }
 
 ajustaTamanhoPalcoJogo();
+
+function posicaoRandomica() {
+    //Gerando posições randomicas para o objeto mosca
+
+    var posicaoX = Math.floor(Math.random() * largura) - 90;
+    var posicaoY = Math.floor(Math.random() * altura) - 90;
+
+    posicaoX = posicaoX < 0 ? 0 : posicaoX;
+    posicaoY = posicaoY < 0 ? 0 : posicaoY;
+
+    console.log(posicaoX, posicaoY);
+
+    //Criando o elemento HTML
+
+    var mosca = document.createElement('img');
+
+    mosca.src = 'img/mosca.png';
+    mosca.className = 'mosca1';
+    mosca.style.left = posicaoX + 'px';
+    mosca.style.top = posicaoY + 'px';
+    mosca.style.position = 'absolute';
+
+    document.body.appendChild(mosca);
     
+}
+
